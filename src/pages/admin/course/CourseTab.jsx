93cf5567a30c1df8daebf89c238b989a005808aa
Abhishek Sharma <abhisheksharmaword@gmail.com>
2025-10-08 18:35:48 +0530

    Update edit page

--- src/pages/admin/course/CourseTab.jsx
+++ src/pages/admin/course/CourseTab.jsx
@@ -7,9 +7,20 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import React, { useState } from "react";
+import { Loader2 } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 const CourseTab = () => {
   const [input, setInput] = useState({
@@ -19,14 +30,42 @@ const CourseTab = () => {
     category: "",
     courseLevel: "",
     courseThumbnail: "",
+    coursePrice: "",
   });
+
+  const [previewThumbnail, setPreviewThumbnail] = useState("");
+
+  const navigate = useNavigate();
 
   const changeEventHandler = (e) => {
     const { name, value } = e.target;
     setInput({ ...input, [name]: value });
   };
 
+  const selectCategory = (value) => {
+    setInput({ ...input, category: value });
+  };
+  const selectCourseLevel = (value) => {
+    setInput({ ...input, courseLevel: value });
+  };
+
+  // get file
+  const selectThumbnail = (e) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      setInput({ ...input, courseThumbnail: file });
+      const fileReader = new FileReader();
+      fileReader.onloadend = () => setPreviewThumbnail(fileReader.result);
+      fileReader.readAsDataURL(file);
+    }
+  };
+
+  const updateCourseHandler = () => {
+    console.log(input);
+  };
+
   const isPublished = false;
+  const isLoading = false;
   return (
     <Card>
       <CardHeader className="flex flex-row justify-between">
@@ -68,6 +107,102 @@ const CourseTab = () => {
           <div>
             <Label>Description</Label>
             <RichTextEditor input={input} setInput={setInput}></RichTextEditor>
+          </div>
+          <div className="flex items-center gap-5">
+            <div>
+              <Label>Category</Label>
+              <Select onValueChange={selectCategory}>
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="Select a category" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Category</SelectLabel>
+                    <SelectItem value="frontend">
+                      Frontend Development
+                    </SelectItem>
+                    <SelectItem value="backend">Backend Development</SelectItem>
+                    <SelectItem value="fullstack">
+                      Fullstack Development
+                    </SelectItem>
+                    <SelectItem value="devops">DevOps</SelectItem>
+                    <SelectItem value="docker">Docker</SelectItem>
+                    <SelectItem value="html-css">HTML & CSS</SelectItem>
+                    <SelectItem value="react">React JS</SelectItem>
+                    <SelectItem value="nextjs">Next JS</SelectItem>
+                    <SelectItem value="datascience-ai">
+                      Data Science & AI
+                    </SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+            <div>
+              <Label>Course Level</Label>
+              <Select onValueChange={selectCourseLevel}>
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="Select a course level" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Course Level</SelectLabel>
+                    <SelectItem value="beginner">Beginner</SelectItem>
+                    <SelectItem value="medium">Medium</SelectItem>
+                    <SelectItem value="advance">Advance</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+            <div>
+              <Label>Price in (INR)</Label>
+              <Input
+                type="number"
+                placeholder="Enter course price"
+                value={input.coursePrice}
+                onChange={changeEventHandler}
+                name="coursePrice"
+                className="w-fit"
+              />
+            </div>
+          </div>
+          <div>
+            <Label>Course Thumbnail</Label>
+            <Input
+              type="file"
+              accept="image/*"
+              className="w-fit cursor-pointer"
+              onChange={selectThumbnail}
+            />
+            {previewThumbnail && (
+              <img
+                src={previewThumbnail}
+                alt="Course Thumbnail"
+                className="w-64 my-2"
+              />
+            )}
+          </div>
+          <div className="flex gap-2">
+            <Button
+              variant="outline"
+              className="cursor-pointer"
+              onClick={() => navigate("/admin/course")}
+            >
+              Cancel
+            </Button>
+            <Button
+              onClick={updateCourseHandler}
+              disabled={isLoading}
+              className="cursor-pointer"
+            >
+              {isLoading ? (
+                <>
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                  Please wait
+                </>
+              ) : (
+                "Save"
+              )}
+            </Button>
           </div>
         </div>
       </CardContent>
